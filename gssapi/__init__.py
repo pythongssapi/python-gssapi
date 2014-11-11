ac2b56b299b0540fd8dd11b8564cea07bbeb667e
Robbie Harwood (frozencemetery) <rharwood@redhat.com>
2014-11-11 16:51:04 -0500

    Import related things when gssapi is itself imported.

---
+++ gssapi/__init__.py
@@ -0,0 +1,7 @@
+from gssapi.raw.types import NameType, RequirementFlag, AddressType  # noqa
+from gssapi.raw.types import MechType, IntEnumFlagSet  # noqa
+from gssapi.raw.oids import OID  # noqa
+
+from gssapi.creds import Credentials  # noqa
+from gssapi.names import Name  # noqa
+from gssapi.sec_contexts import SecurityContext  # noqa
