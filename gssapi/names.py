336ffb1723d37c828ac3e6d7fdbb2df83f94e42f
Solly Ross <sross@redhat.com>
2014-12-09 11:51:28 -0500

    Document Name class
    This commit adds documentation for the Name class.
    
    Part of #7

--- gssapi/names.py
+++ gssapi/names.py
@@ -6,9 +6,38 @@ from gssapi import _utils
 
 
 class Name(rname.Name):
+    """GSSAPI Name
+
+    This class represents a GSSAPI name which may be used with
+    used with and/or returned by other GSSAPI methods.
+
+    It inherits from the low-level GSSAPI :class:`~gssapi.raw.names.Name`
+    class, and thus may used with both low-level and high-level API methods.
+
+    This class may be pickled and unpickled, as well as copied.
+
+    The :func:`str` and :func:`bytes` methods may be used to retrieve the
+    text of the name.
+    """
+
     __slots__ = ()
 
     def __new__(cls, base=None, name_type=None, token=None):
+        """Create or import a GSSAPI name
+
+        The constructor either creates or imports a GSSAPI name.
+
+        If a :python:`~gssapi.raw.names.Name` object from the low-level API
+        is passed as the `base` argument, it will be converted into a
+        high-level object.
+
+        If the `token` argument is used, the name will be imported using
+        the token.
+
+        Otherwise, a new name will be created, using the `base` argument as
+        the string and the `name_type` argument to denote the name type.
+        """
+
         if token is not None:
             base_name = rname.import_name(token, NameType.export)
         elif isinstance(base, rname.Name):
@@ -39,6 +68,7 @@ class Name(rname.Name):
 
     @property
     def name_type(self):
+        """Get the name type of this name"""
         return rname.display_name(self, name_type=True).name_type
 
     def __eq__(self, other):
@@ -58,9 +88,30 @@ class Name(rname.Name):
                                                   name_type=disp_res.name_type)
 
     def export(self):
+        """Export the name
+
+        This method exports the name into a byte string which can then be
+        imported by using the `token` argument of the constructor.
+
+        Returns:
+            bytes: the exported name in token form
+        """
+
         return rname.export_name(self)
 
     def canonicalize(self, mech_type):
+        """Canonicalize a name with respect to a mechanism
+
+        This method returns a new Name that is canonicalized according to
+        the given mechanism.
+
+        Args:
+            mech_type (OID): the mechanism type to use
+
+        Returns:
+            Name: the canonicalized name
+        """
+
         return type(self)(rname.canonicalize_name(self, mech_type))
 
     def __copy__(self):
