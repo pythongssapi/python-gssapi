25a13b5a7672cae7f38a5b6c968c398f5d903a79
Robbie Harwood (frozencemetery) <rharwood@redhat.com>
2015-03-19 14:52:14 -0400

    [travis] Show all test statuses in nosetests
    This shows us which tests are passed, failing, or - most importantly - have
    been skipped in Travis.  The extra verbosity is intentionally not propogated
    to local `tox` invocations as it is easy for local users to add in.

--- .travis.sh
+++ .travis.sh
@@ -9,7 +9,7 @@ F8_PY=$?
 flake8 gssapi --filename='*.pyx,*.pxd' --ignore=E225,E226,E227,E901,E402
 F8_MAIN_CYTHON=$?
 
-python setup.py nosetests
+python setup.py nosetests --verbosity=3
 TEST_RES=$?
 
 if [ $F8_SETUP -eq 0 -a $F8_PY -eq 0 -a $F8_MAIN_CYTHON -eq 0 -a $TEST_RES -eq 0 ]; then
