77a619d5d1ce49514a20148d65d5b49982197322
Solly Ross <sross@redhat.com>
2015-03-25 14:16:32 -0400

    Update the deploy script directory name
    Previously, the deploy script generated a tarball with a directory name
    of 'python-gssapi-${TAG_NAME}', giving 'python-gssapi-v${VERSION}'.
    However, to keep compatibility with the GitHub tarball and with the
    general naming convention, the top-level directory should be
    'python-gssapi-${VERSION}' (no 'v' before the version).
    
    This commit corrects that.

--- .travis.before-deploy.sh
+++ .travis.before-deploy.sh
@@ -7,5 +7,14 @@ git clean -Xdf
 mkdir ./tag_build
 
 # create and checksum the tarball
-tar -czvf ./tag_build/${TRAVIS_TAG}.tar.gz --exclude='tag_build' --exclude='.git' --transform "s,^\.,python-gssapi-${TRAVIS_TAG}," .
+
+# no bashisms for portability
+if [ x"${TRAVIS_TAG#v[0-9]}" = "x${TRAVIS_TAG}" ]; then
+    PYTHON_GSSAPI_VERSION=${TRAVIS_TAG}
+else
+    PYTHON_GSSAPI_VERSION=${TRAVIS_TAG#v}
+fi
+
+
+tar -czvf ./tag_build/${TRAVIS_TAG}.tar.gz --exclude='tag_build' --exclude='.git' --transform "s,^\.,python-gssapi-${PYTHON_GSSAPI_VERSION}," .
 sha512sum --binary ./tag_build/${TRAVIS_TAG}.tar.gz > ./tag_build/${TRAVIS_TAG}.sha512sum
