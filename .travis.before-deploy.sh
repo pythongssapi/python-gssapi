51bd27c7fb4de362fef31bd8fcbe98fab72372c7
Solly Ross <sross@redhat.com>
2015-04-07 12:58:59 -0400

    Generate SHA-512 checksums for releases
    This commit makes it so that we upload SHA-512 checksums instead of
    MD5 checksums with release tarballs.

--- .travis.before-deploy.sh
+++ .travis.before-deploy.sh
@@ -8,4 +8,4 @@ mkdir ./tag_build
 
 # create and checksum the tarball
 tar -czvf ./tag_build/${TRAVIS_TAG}.tar.gz --exclude='tag_build' --exclude='.git' --transform "s,^\.,python-gssapi-${TRAVIS_TAG}," .
-md5sum --binary ./tag_build/${TRAVIS_TAG}.tar.gz > ./tag_build/${TRAVIS_TAG}.md5sum
+sha512sum --binary ./tag_build/${TRAVIS_TAG}.tar.gz > ./tag_build/${TRAVIS_TAG}.sha512sum
