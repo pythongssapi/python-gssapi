bd98bd62ae6426621228845d11f48c9dc473826d
Solly Ross <sross@redhat.com>
2015-04-07 13:33:20 -0400

    Name release tarballs "python-gssapi-$VER"
    This commit changes the name of release tarballs from "v${VER}" to
    "python-gssapi-${VER}".

--- .travis.before-deploy.sh
+++ .travis.before-deploy.sh
@@ -15,6 +15,7 @@ else
     PYTHON_GSSAPI_VERSION=${TRAVIS_TAG#v}
 fi
 
+PKG_NAME_VER="python-gssapi-${PYTHON_GSSAPI_VERSION}"
 
-tar -czvf ./tag_build/${TRAVIS_TAG}.tar.gz --exclude='tag_build' --exclude='.git' --transform "s,^\.,python-gssapi-${PYTHON_GSSAPI_VERSION}," .
-sha512sum --binary ./tag_build/${TRAVIS_TAG}.tar.gz > ./tag_build/${TRAVIS_TAG}.sha512sum
+tar -czvf ./tag_build/${PKG_NAME_VER}.tar.gz --exclude='tag_build' --exclude='.git' --transform "s,^\.,${PKG_NAME_VER}," .
+sha512sum --binary ./tag_build/${PKG_NAME_VER}.tar.gz > ./tag_build/${PKG_NAME_VER}.sha512sum
