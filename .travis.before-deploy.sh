bd0a9ab9e6ab53ce149a0189e020d32a80fff792
Solly Ross <sross@redhat.com>
2015-04-22 10:48:45 -0400

    Automatically Upload to PyPI
    This commit automatically uploads releases to PyPI under the same
    conditions that would trigger an upload of a tarball/checksum to
    GitHub releases (docs are uploaded too).

--- .travis.before-deploy.sh
+++ .travis.before-deploy.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+# build the docs
+# the first run is for the docs build, so don't clean up
+pip install -r docs-requirements.txt
+
+# install dependencies so that sphinx doesn't have issues
+# (this actually just installs the whole package in dev mode)
+pip install -e .
+
+# place in a non-standard location so that they don't get cleaned up
+python setup.py build_sphinx --build-dir travis_docs_build
+
+# for the tarball upload
 # clean up
 git clean -Xdf
+
+# until this gets fixed in dpl
+rm setuptools-*.zip
 
 # make the dir
 mkdir ./tag_build
@@ -17,8 +32,5 @@ fi
 
 PKG_NAME_VER="python-gssapi-${PYTHON_GSSAPI_VERSION}"
 
-tar -czvf ./tag_build/${PKG_NAME_VER}.tar.gz --exclude='tag_build' --exclude='.git' --transform "s,^\.,${PKG_NAME_VER}," .
+tar -czvf ./tag_build/${PKG_NAME_VER}.tar.gz --exclude='tag_build' --exclude='.git' --exclude='travis_docs_build' --exclude='.git' --transform "s,^\.,${PKG_NAME_VER}," .
 sha512sum --binary ./tag_build/${PKG_NAME_VER}.tar.gz > ./tag_build/${PKG_NAME_VER}.sha512sum
-
-# for the docs deploy
-pip install -r test-requirements.txt
--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,17 @@
 #!/bin/sh -x
 
+# always build in-place so that Sphinx can find the modules
+python setup.py build_ext --inplace
+BUILD_RES=$?
+
 if [ x"$KRB5_VER" = "xheimdal" ]; then
-    python setup.py build_ext --inplace
-    exit $?
+    # heimdal can't run the tests yet, so just exit
+    exit $BUILD_RES
+fi
+
+if [ $BUILD_RES -ne 0 ]; then
+    # if the build failed, don't run the tests
+    exit $BUILD_RES
 fi
 
 flake8 setup.py
