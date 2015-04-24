frozencemetery/high-level-port-prototype

High-Level Documentation Updates
This commit updates the high-level documentation.  Typos were
corrected, improvements were made for readability, and more
links were inserted.

Additionally, two custom Sphinx extensions were added to help
with the process:

The first extension catches unknown class references and attempts
to track them down.  The class is searched for as if it were an
`:any:` reference.  The results from this searched are used, prefering
results in the current API level (high-level vs low-level).

The second extension adds a convinience role, `:requires-ext`, which
inserts a link and some text indicating that a particular extension
is required.  If the extension is an RFC, a link to the RFC is
generated, otherwise, a link to the low-level API module is generated.
In the latter case, the text of the link is generated using the first
line of the docstring of the low-level module, if it is present.
Otherwise, the partial name of the module is used (the '%s' part
of 'ext_%s')

Finally, as per above, the docstrings of several extensions were
updated.

docs/custom_extensions/requires_rfc.py
@@ -1,0 +1,62 @@
+import sys
+
+from docutils import nodes, statemachine
+from docutils.parsers.rst.directives import admonitions
+from docutils.parsers.rst.roles import set_classes
+from docutils.parsers.rst import roles
+from sphinx.domains import python as py_domain
+
+def setup(app):
+    app.add_role('requires-ext', RequiresExtRole(app))
+
+
+class RequiresExtRole(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, name, rawtext, text, lineno, inliner,
+                 options={}, content=[]):
+        if text.startswith('rfc'):
+            rfc_text = text[3:]
+
+            rfc_node, rfc_msg = roles.rfc_reference_role(
+                'rfc', ':rfc:`%s`' % rfc_text, rfc_text, lineno,
+                inliner, options, content)
+
+            if rfc_msg:
+                # error
+                return (rfc_node, rfc_message)
+            else:
+                middle_parts = rfc_node + [nodes.Text(" extension",
+                                                         " extension")]
+        else:
+            ext_name = 'gssapi.raw.ext_%s' % text
+            # autodoc has already imported everything
+            try:
+                ext_module = sys.modules[ext_name]
+            except KeyError:
+                ext_title = text + " extension"
+            else:
+                if ext_module.__doc__:
+                    ext_title = ext_module.__doc__.splitlines()[0]
+                else:
+                    ext_title = text + " extension"
+            ref_nodes, ref_messages = self.app.env.domains['py'].role('mod')(
+                'mod', rawtext, ext_name, lineno, inliner)
+
+            if ref_messages:
+                # error
+                return (ref_nodes, ref_messages)
+
+            title_node = nodes.Text(ext_title, ext_title)
+
+            ref_nodes[0].children = [title_node]
+
+            middle_parts = ref_nodes
+
+        begin_text = nodes.Text("requires the ", "requires the ")
+
+        main_nodes = [begin_text] + middle_parts
+        wrapper_node = nodes.emphasis('', '', *main_nodes)
+
+        return ([nodes.Text('', ''), wrapper_node, nodes.Text('', '')], [])

pythongssapi/python-gssapi

docs/custom_extensions/requires_rfc.py
@@ -1,0 +1,60 @@
+import sys
+
+from docutils import nodes
+from docutils.parsers.rst import roles
+
+
+def setup(app):
+    app.add_role('requires-ext', RequiresExtRole(app))
+
+
+class RequiresExtRole(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, name, rawtext, text, lineno, inliner,
+                 options={}, content=[]):
+        if text.startswith('rfc'):
+            rfc_text = text[3:]
+
+            rfc_node, rfc_msg = roles.rfc_reference_role(
+                'rfc', ':rfc:`%s`' % rfc_text, rfc_text, lineno,
+                inliner, options, content)
+
+            if rfc_msg:
+                # error
+                return (rfc_node, rfc_msg)
+            else:
+                middle_parts = rfc_node + [nodes.Text(" extension",
+                                                      " extension")]
+        else:
+            ext_name = 'gssapi.raw.ext_%s' % text
+            # autodoc has already imported everything
+            try:
+                ext_module = sys.modules[ext_name]
+            except KeyError:
+                ext_title = text + " extension"
+            else:
+                if ext_module.__doc__:
+                    ext_title = ext_module.__doc__.splitlines()[0]
+                else:
+                    ext_title = text + " extension"
+            ref_nodes, ref_messages = self.app.env.domains['py'].role('mod')(
+                'mod', rawtext, ext_name, lineno, inliner)
+
+            if ref_messages:
+                # error
+                return (ref_nodes, ref_messages)
+
+            title_node = nodes.Text(ext_title, ext_title)
+
+            ref_nodes[0].children = [title_node]
+
+            middle_parts = ref_nodes
+
+        begin_text = nodes.Text("requires the ", "requires the ")
+
+        main_nodes = [begin_text] + middle_parts
+        wrapper_node = nodes.emphasis('', '', *main_nodes)
+
+        return ([nodes.Text('', ''), wrapper_node, nodes.Text('', '')], [])