beff154dca07cdced26a4da3625a22d1dee68d5e
Robbie Harwood <rharwood@redhat.com>
2017-03-27 13:35:12 -0400

    Make flake work against cython files again

--- .travis.sh
+++ .travis.sh
@@ -20,7 +20,14 @@ F8_SETUP=$?
 flake8 gssapi
 F8_PY=$?
 
-flake8 gssapi --filename='*.pyx,*.pxd' --ignore=E225,E226,E227,E901,E402
+# Cython requires special flags since it is not proper Python
+# E225: missing whitespace around operator
+# E226: missing whitespace around arithmetic operator
+# E227: missing whitespace around bitwise or shift operator
+# E402: module level import not at top of file
+# E901: SyntaxError or IndentationError
+# E999: Internal AST compilation error (flake8 specific)
+flake8 gssapi --filename='*.pyx,*.pxd' --ignore=E225,E226,E227,E402,E901,E999
 F8_MAIN_CYTHON=$?
 
 python setup.py nosetests --verbosity=3
