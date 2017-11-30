3b2fb37bfd59d3879f7e2813f325d4810a48efc2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-11-29 22:19:18 -0500

    Publish documentation to GitHub Pages
    The old pythonhosted.org docs hosting is deprecated now, so we needed
    a new hosting site.  Unfortunately, RTD can't handle our use cases
    (customizations, requiring C libraries).  Instead, we use GitHub pages.
    
    Every push to master (or an `infra/` branch for testing) will push
    documentation to the `/latest` subdirectory of the gh-pages site,
    while every tag build will push to the `/stable` directory.

---
+++ .travis/docs-deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -ex
+
+# NB (very important): BE VERY CAREFUL WITH `set -x` FOR THIS FILE.
+# The GitHub token is sensative information, and should never
+# be displayed on in the clear.
+
+source_directory=${1?need <source dir> <target dir> <target repo> [<target branch, default: gh-pages>]}
+target_directory=${2?need <source dir> <target dir> <target repo> [<target branch, default: gh-pages>]}
+target_repo=${3?need <source dir> <target dir> <target repo> [<target branch, default: gh-pages>]}
+target_branch=${4:-gh-pages}
+
+desc=$(git describe --tags)
+
+scratch_dir=$(mktemp -d)
+
+set +x # IMPORTANT
+echo "cloning https://<elided>@github.com/${target_repo}.git#${target_branch} in to ${scratch_dir}/docs..."
+git clone https://${GITHUB_TOKEN}@github.com/${target_repo}.git ${scratch_dir}/docs -b ${target_branch}
+set -x
+
+mkdir -p ${scratch_dir}/docs/${target_directory}
+cp -r ${source_directory}/. ${scratch_dir}/docs/${target_directory}
+echo $desc > ${scratch_dir}/docs/${target_directory}/.from
+pushd $scratch_dir/docs
+git config user.email "deploy@travis-ci.org"
+git config user.name "Deployment Bot (from Travis CI)"
+git add ${target_directory}
+git commit -m "Update ${target_directory} docs in based on ${desc}"
+
+set +x # IMPORTANT
+echo "pushing to https://<elided>@github.com/${target_repo}.git#${target_branch}"
+git push --quiet --force-with-lease origin ${target_branch}:${target_branch}
+set -x
+
+popd
+rm -rf ${scratch_dir}
+echo "done!"
