b24de7d6fd901353d322f53ed311a3f1a4437dff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-18 21:28:40 +0000

    Update stable docs in based on v0.0.0-test5

--- stable/_static/documentation_options.js
+++ stable/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0',
+    VERSION: '0.0.0-test5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
