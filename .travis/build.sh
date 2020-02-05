6aa0f4f25e1186e2c890daaa647738e7b40988a1
Robbie Harwood <rharwood@redhat.com>
2020-02-05 15:49:21 -0500

    [travis] Merge "verify" and "test" stages

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,6 +4,28 @@
 source ./.travis/lib-setup.sh
 setup::install
 
+if [ x"$FLAKE" = "xyes" ]; then
+    flake8 setup.py
+    F8_SETUP=$?
+
+    flake8 gssapi
+    F8_PY=$?
+
+    # Cython requires special flags since it is not proper Python:
+    # - E225: missing whitespace around operator
+    # - E226: missing whitespace around arithmetic operator
+    # - E227: missing whitespace around bitwise or shift operator
+    # - E402: module level import not at top of file (needed for the `GSSAPI="blah" lines)
+    # - E901: SyntaxError or IndentationError
+    # - E999: Internal AST compilation error (flake8 specific)
+    flake8 gssapi --filename='*.pyx,*.pxd' --ignore=E225,E226,E227,E402,E901,E999
+    F8_MAIN_CYTHON=$?
+
+    if [ $F8_SETUP -ne 0 -o $F8_PY -ne 0 -o $F8_MAIN_CYTHON -ne 0 ]; then
+        exit 1
+    fi
+fi
+
 # always build in-place so that Sphinx can find the modules
 python setup.py build_ext --inplace $EXTRA_BUILDEXT
 BUILD_RES=$?
