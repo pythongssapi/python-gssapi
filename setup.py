dd6892f591f461d5d882be2c532da265b511c968
Robbie Harwood <rharwood@redhat.com>
2020-04-07 13:37:29 -0400

    Prepare for 1.6.3 Release
    Updated version in setup.py and in docs conf.py.

--- setup.py
+++ setup.py
@@ -295,7 +295,7 @@ if sys.version_info < (3, 4):
 
 setup(
     name='gssapi',
-    version='1.6.3',
+    version='1.6.4',
     author='The Python GSSAPI Team',
     author_email='rharwood@redhat.com',
     packages=['gssapi', 'gssapi.raw', 'gssapi.raw._enum_extensions',
