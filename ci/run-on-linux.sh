d8858319bf0bf5a629b5d15d66fbc06fafef9a76
Jordan Borean <jborean93@gmail.com>
2022-08-12 11:48:49 +1000

    Fix up build problems with latest change
    Signed-off-by: Jordan Borean <jborean93@gmail.com>

--- ci/run-on-linux.sh
+++ ci/run-on-linux.sh
@@ -3,7 +3,7 @@
 docker run \
     --rm \
     --hostname test.krbtest.com \
-    --volume "$( pwd )":/tmp/build:z \
+    --volume "$( pwd )":/tmp/build \
     --workdir /tmp/build \
     --env KRB5_VER=${KRB5_VER:-mit} \
     --env FLAKE=${FLAKE:no} \
