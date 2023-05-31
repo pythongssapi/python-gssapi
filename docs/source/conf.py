e9e2021b7a76317d19e210d56a50424786f8c5e8
Jordan Borean <jborean93@gmail.com>
2023-05-31 13:00:43 +1000

    Use OIDC for PyPI release
    Signed-off-by: Jordan Borean <jborean93@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -316,7 +316,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
 
 # which docstring to use for the class
 # can be 'class', 'init', or 'both'
